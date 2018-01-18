c7cdf2e942c6e7827a42afa005c9a7c8a4fa026d
Eric Ahlstrom <eric@ericahlstrom.com>
2018-01-18 14:34:33 -0800

    Original upload

---
+++ Assets/Scripts/DrumStickHandler.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.WSA.Input;
+
+public class DrumStickHandler : MonoBehaviour {
+
+    public bool isRightHand = false;
+
+    private Vector3 controllerPos, controllerForward;
+    private Quaternion controllerRot;
+
+    // Use this for initialization
+    void Start () {
+        //InteractionManager.SourceDetected += InteractionManager_SourceDetected;
+        InteractionManager.InteractionSourceUpdated += InteractionManager_InteractionSourceUpdated;
+        //InteractionManager.SourceLost += InteractionManager_SourceLost;
+        //InteractionManager.SourcePressed += InteractionManager_SourcePressed;
+        //InteractionManager.SourceReleased += InteractionManager_SourceReleased;
+    }
+
+    private void InteractionManager_InteractionSourceUpdated(InteractionSourceUpdatedEventArgs obj)
+    {
+        var reading = InteractionManager.GetCurrentReading();
+        foreach (var sourceState in reading)
+        {
+            var sourcePose = sourceState.sourcePose;
+            Vector3 position = Vector3.zero;
+            Quaternion rotation = Quaternion.identity;
+            bool hasRotation, hasPosition;
+
+            if (isRightHand && sourceState.source.handedness == InteractionSourceHandedness.Right)
+            {
+                hasRotation = sourcePose.TryGetRotation(out rotation, InteractionSourceNode.Grip);
+                hasPosition = sourcePose.TryGetPosition(out position, InteractionSourceNode.Grip);
+            }
+            else if (!isRightHand && sourceState.source.handedness == InteractionSourceHandedness.Left)
+            {
+                hasRotation = sourcePose.TryGetRotation(out rotation, InteractionSourceNode.Grip);
+                hasPosition = sourcePose.TryGetPosition(out position, InteractionSourceNode.Grip);
+            }
+            else
+            {
+                hasRotation = false;
+                hasPosition = false;
+            }
+
+            if (hasRotation || hasPosition)
+            {
+                transform.rotation = rotation;
+                transform.position = position;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/DrumTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrumTrigger : MonoBehaviour
+{
+
+    public AudioClip drumSound;
+    AudioSource audioSource;
+
+    private float fxTiimer;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (fxTiimer < 0)
+        {
+            audioSource.PlayOneShot(drumSound, 0.7F);
+            fxTiimer = 0.1f;
+        }
+    }
+
+    private void Update()
+    {
+        fxTiimer -= Time.deltaTime;
+    }
+}
+
+
